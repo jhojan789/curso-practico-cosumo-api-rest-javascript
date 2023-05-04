687e5f9c410b438e61836a3a3c7664d680216a58
Jhojan Tobón <jhojantobonm@gmail.com>
2023-05-04 18:50:17 -0500

    API Trending movies added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log(API_KEY);
+
+async function getTrendingMoviesPreview(){
+  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
+  const data = await res.json();
+  const movies = data.results;
+  const trendingPreview = document.querySelector('.trendingPreview-movieList');
+  movies.forEach(movie => {
+      trendingPreview.innerHTML+= 
+      `
+      <div class="movie-container">
+      <img
+        src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+        class="movie-img"
+        alt=${movie.title}
+      />
+    </div>
+    `;
+  });
+
+}
+
+getTrendingMoviesPreview();
